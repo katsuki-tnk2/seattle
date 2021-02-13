bd80d5e3cef0c1c2f888083365d50ab473ae3a82
katsuki-tnk2 <78991559+katsuki-tnk2@users.noreply.github.com>
2021-02-13 10:56:54 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+$(function(){
+	$('.title').fadeIn(3000);
+
+	$('.menu-icon').click(function() {
+		if($('.modal-menu ul').hasClass('in')){
+			$('.modal-menu ul').css('display', 'block');
+			$('.modal-menu ul').removeClass('in');
+		} else {
+			$('.modal-menu ul').addClass('in');
+			$('.modal-menu ul').css('display', 'none');
+	
+		};
+	});
+	
+});
---
+++ university/main_university.js
@@ -0,0 +1,14 @@
+$(function(){
+
+$('.menu-icon').click(function() {
+	if($('.modal-menu ul').hasClass('in')){
+		$('.modal-menu ul').css('display', 'block');
+		$('.modal-menu ul').removeClass('in');
+	} else {
+		$('.modal-menu ul').addClass('in');
+		$('.modal-menu ul').css('display', 'none');
+
+	};
+});
+
+});
